6e9583b08fd19c76f8051ad417ec8cee2549aefe
Kevin Robert Stravers <macocio@gmail.com>
2017-03-13 16:04:06 +0100

    Add the plots for 10.2.3

---
+++ plot2s3.m
@@ -0,0 +1,17 @@
+figure;
+subplot(3,2,1);
+plot(travel.time, travel.signals.values);
+title('travel');
+subplot(3,2,2);
+plot(travel_rate.time, travel_rate.signals.values);
+title('travel rate');
+subplot(3,2,3);
+plot(pitch.time, pitch.signals.values);
+title('pitch');
+subplot(3,2,4);
+plot(pitch_rate.time, pitch_rate.signals.values);
+title('pitch rate');
+subplot(3,2,[5 6]);
+plot(u(:,1), u(:,2));
+title('u');
+xlabel(strcat('time, q=', num2str(q)));
